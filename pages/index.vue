a873e4642a4fafe90dfbf86ceaf4a899ecaf31f4
Valentin Cernuta <vcernuta3@gmail.com>
2022-06-02 19:36:23 +0200

    Refactor project with home-made CSS

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,181 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <h1>Hello World</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+      reiciendis ipsa voluptatum exercitationem nemo, eligendi, ducimus culpa
+      saepe accusantium quam sed est quis architecto eum voluptate, quod quia
+      beatae dolore!
+    </p>
+  </div>
 </template>
 
 <script>
