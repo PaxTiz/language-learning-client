0c2133edeb0055cd708f984c2438cab2e59f180c
Valentin Cernuta <vcernuta3@gmail.com>
2022-06-18 20:24:15 +0200

    feat: add delete modal component

---
+++ components/modals/DeleteModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-if="show" class="modal-container">
+    <div class="modal">
+      <div class="modal-heading">
+        <h2 class="modal-title">{{ title }}</h2>
+        <p v-if="subtitle">{{ subtitle }}</p>
+      </div>
+
+      <div class="modal-actions">
+        <button class="primary" @click="confirm">{{ yes }}</button>
+        <button class="danger" @click="cancel">{{ no }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DeleteModal',
+
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: () => null,
+    },
+    yes: {
+      type: String,
+      default: () => 'Yes, I want',
+    },
+    no: {
+      type: String,
+      default: () => 'No, cancel this action',
+    },
+  },
+
+  methods: {
+    confirm() {
+      this.$emit('ok')
+    },
+    cancel() {
+      this.$emit('cancel')
+    },
+  },
+}
+</script>
