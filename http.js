fbd7b3619a0b821a4858b082f9ae23fadb097893
Valentin Cernuta <vcernuta3@gmail.com>
2022-06-18 18:15:45 +0200

    fix: improve http error handling

--- http.js
+++ http.js
@@ -7,9 +7,12 @@ const http = axios.create({
 http.interceptors.response.use(response => {
     if (process.server && process.env.NODE_ENV !== 'production' && response) {
         /* eslint-disable no-console */
-        console.log(response.status, response.config.url)
+        console.log(response.config.method.toUpperCase(), response.status, response.config.url)
     }
+    if (response.status >= 400) throw response
     return response.data
+}, error => {
+    throw error.response.data
 })
 
 
